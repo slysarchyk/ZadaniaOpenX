110d89d7a185349f991aae7f4fdad6aabb300359
slysarchyk <58818048+slysarchyk@users.noreply.github.com>
2020-03-26 19:12:29 +0100

    The program was reworked by methods.

---
+++ ZadaniaOpenX.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZadaniaOpenX.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZadaniaOpenX.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d74ee9de-8575-42a0-87ba-d5b16f7518f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ZadaniaOpenX/Program.cs
+++ ZadaniaOpenX/Program.cs
@@ -12,19 +12,48 @@ namespace ZadaniaOpenX
     {
         static void Main(string[] args)
         {
-            //1 Зчитати з файлу і десереалізувати дані
-            string userJson = File.ReadAllText(@"C:\Users\Admin\source\repos\ZadaniaOpenX\ZadaniaOpenX\Data\JSON\Users.json");
-            string postJson = File.ReadAllText(@"C:\Users\Admin\source\repos\ZadaniaOpenX\ZadaniaOpenX\Data\JSON\Posts.json");
+            CountPost();
+            UserWritePost();
+            CheckRepeatTitlePost();
+            ClosestUser();
+        }
 
+        static List<User> DeserealizeUser()
+        {
+            string userJson = File.ReadAllText(@"C:\Users\Admin\source\repos\ZadaniaOpenX\ZadaniaOpenX\Data\JSON\Users.json");
             List<User> restoredUser = JsonConvert.DeserializeObject<List<User>>(userJson);
+
+            return restoredUser;
+        }
+
+        static List<Post> DeserealizePost()
+        {
+            string postJson = File.ReadAllText(@"C:\Users\Admin\source\repos\ZadaniaOpenX\ZadaniaOpenX\Data\JSON\Posts.json");
             List<Post> restoredPost = JsonConvert.DeserializeObject<List<Post>>(postJson);
 
-            //2 Вивести стільки постів написав кожен юзер
+            return restoredPost;
+        }
+
+        static void CountPost()
+        {
+            var restoredPost = DeserealizePost();
+            Console.WriteLine($"{restoredPost.Count()} - posts have been written");
+        }
+
+        static void UserWritePost()
+        {
+            var restoredUser = DeserealizeUser();
+            var restoredPost = DeserealizePost();
+
             foreach (var item in restoredUser)
                 Console.WriteLine($"{item.UserName} - write - {restoredPost.Count(x => x.UserId == item.Id)} posts");
             Console.ReadLine();
+        }
 
-            //3 Перевірити чи повторяються заголовки, і вивести які повторюються
+        static void CheckRepeatTitlePost()
+        {
+            var restoredPost = DeserealizePost();
+
             List<Post> reapetItem = restoredPost.GroupBy(x => x.Title).
                 Where(x => x.Count() > 1).
                 Select(x => x.First()).
@@ -38,33 +67,22 @@ namespace ZadaniaOpenX
             else
                 Console.WriteLine("No repeating Titles");
             Console.ReadLine();
+        }
 
-            //4 Знвйти для кожного юзера іншого. який найближче живе
+        static void ClosestUser()
+        {
+            var restoredUser = DeserealizeUser();
+
             foreach (var geoitem in restoredUser)
             {
                 Console.WriteLine($"{geoitem.UserName}`s address is in coordinates: Lat - {geoitem.Address.Geo.Lat}, Lng - {geoitem.Address.Geo.Lng}");
-
                 GeoCoordinate geoUser = new GeoCoordinate(geoitem.Address.Geo.Lat, geoitem.Address.Geo.Lng);
-
-                //ця конструкція тільки для перевірки наочної в кінцевому видвлю
-                foreach (var i in restoredUser)
-                {
-                    GeoCoordinate geoUser2 = new GeoCoordinate(i.Address.Geo.Lat, i.Address.Geo.Lng);
-                    var distance = geoUser2.GetDistanceTo(geoUser);
-
-                    List<double> res = new List<double>();
-                    res.Add(distance/1000);
-
-                    Console.WriteLine(distance);
-                }
-                //-------------------------------------------------------------------------
 
                 var minDistance = restoredUser.Select(x => new { Coor = new GeoCoordinate(x.Address.Geo.Lat, x.Address.Geo.Lng), Username = x.UserName }).
                     Where(x => geoUser.GetDistanceTo(x.Coor) > 0).
                     Min(x => geoUser.GetDistanceTo(x.Coor) / 1000);
-                
+
                 Console.WriteLine($"User short distance {minDistance}");
-                Console.WriteLine();
             }
             Console.ReadLine();
         }
